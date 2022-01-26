b8a62feb4d1c5ced11e2a87e3b1127a3eaaf34cc
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-26 10:33:40 -0300

    adicionando axios para fazer requisições

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login'
-import Book from './pages/Book';
+import Books from './pages/Books';
+import NewBook from './pages/NewBook';
 
 export default function RouteApp() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login/>}/>
-                <Route path='/book' element={<Book/>}/>
+                <Route path='/books' element={<Books/>}/>
+                <Route path='/book/new' element={<NewBook/>}/>
             </Routes>
         </BrowserRouter>
     );
