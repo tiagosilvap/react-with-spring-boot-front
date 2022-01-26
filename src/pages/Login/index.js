9c40c0d2e24a740ef1813ea780a120e6b767a311
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-26 16:01:47 -0300

    remove unsed padlock

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './styles.css'
 import logoImage from '../../assets/logo.svg'
-import padlock from '../../assets/padlock.png'
 
 export default function Login() {
     return (
         <div className="login-container">
             <section className="form">
-                <img src={logoImage} alt="logo Image"/>
+                <img src={logoImage} alt="logo App"/>
                 
                 <form>
                     <h1>Access your Account</h1>
