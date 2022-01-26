1fa86fe5dfc150d550ee5abb765764325db5b891
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-25 22:49:48 -0300

    add icon

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-// import { FiPower } from 'react-icons/fi'
+import { FiPower } from 'react-icons/fi'
 
 import "./styles.css";
 import logoImage from '../../assets/logo.svg'
@@ -12,9 +12,9 @@ export default function Book() {
                 <img src={logoImage} alt="Image"/>
                 <span>Welcome, <strong>Tiago</strong>!</span>
                 <Link className="button" to="book/new">Add new Book</Link>
-                {/* <button type="button">
+                <button type="button">
                     <FiPower size={18} color="#251FC5" />
-                </button> */}
+                </button>
             </header>
 
             <h1>Registered Books</h1>
