fc8eaaccbf30e3758e7dac6eb2b5dd9ad976e503
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-27 10:00:32 -0300

    realizando login de autenticação com axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import './styles.css'
 import logoImage from '../../assets/logo.svg'
 
+import api from '../../services/api'
+
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function login(e) {
+        e.preventDefault();
+
+        const data = {
+            username,
+            password
+        };
+        try {
+            const response = await api.post('/auth/signin', data);
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+            navigate('/books');
+        } catch(err) {
+            alert('Login failed! Try agains!');
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImage} alt="logo App"/>
                 
-                <form>
+                <form onSubmit={login}>
                     <h1>Access your Account</h1>
-                    <input placeholder="Username" />
-                    <input type="password" placeholder="Password" />
+                    <input 
+                        placeholder="Username" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input 
+                        type="password" placeholder="Password" 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Login</button>
                 </form>
